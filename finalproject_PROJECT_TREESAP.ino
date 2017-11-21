b2fe69610ac65f59019ea93f0b4ef53179b5fd16
treesapelectronics <33070706+treesapelectronics@users.noreply.github.com>
2017-11-21 15:21:07 -0500

    UPDATE IV
    Allows the Joystick to select an LED to light up

--- finalproject_PROJECT_TREESAP.ino
+++ finalproject_PROJECT_TREESAP.ino
@@ -1,12 +1,64 @@
-int jX = 33;
+#include "LedControl.h"
+const int SW_pin = 36;
+boolean grid[8][8] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+};
+boolean bState = 0;
+boolean lbState = 0;
+int lastCheck = 0;
+int checkInt = 20;
+
+LedControl lc = LedControl(12, 11, 10, 1); //10 is to CLOCK, 9 = CS, 8=DIN//
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
-  // put your setup code here, to run once:
-
+  pinMode(SW_pin, INPUT);
+  digitalWrite(SW_pin, HIGH);
+  Serial.begin(9600);
+  lc.shutdown(0, false); // turn off power saving, enables display
+  lc.setIntensity(0, 8); // sets brightness (0~15 possible values)
+  lc.clearDisplay(0);// clear screen
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  const int SW_pin = 36;
+  const int UD = analogRead(A16);
+  const int LR = analogRead(A15);
+  int x_translate = map(LR, 1023, 0, 7, -1); //This maps the values//
+  int y_translate = map(UD, 0, 1023, 0, 7);
+  drawGrid();
+  lc.setLed(0, x_translate, y_translate, true);
+  delay(20); //Mess with this delay to get your joystick correct//
+  drawGrid();
+  lc.setLed(0, x_translate, y_translate, false);
+  delay(20); //Mess with this delay to get your joystick correct//
+}
+void drawGrid() {
 
+  const int UD = analogRead(A16);
+  const int LR = analogRead(A15);
+  int x_translate = map(LR, 1023, 0, 7, -1); //This maps the values//
+  int y_translate = map(UD, 0, 1023, 0, 7);
+  if (millis() < lastCheck + checkInt) return;
+
+  lastCheck = millis();
+
+  lbState = bState;
+  bState = digitalRead(SW_pin);
+
+  for (int i = 0; i <= 7; i++) {
+    for (int j = 0; j <= 7; j++) {
+      if (lbState == HIGH && bState == LOW) {
+        grid[x_translate][y_translate] = 1;
+        lc.setLed(0, x_translate, y_translate, true);
+
+      }
+    }
+  }
 }
