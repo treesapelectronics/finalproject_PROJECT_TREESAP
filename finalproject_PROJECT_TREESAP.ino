a8ab0f7061dc274c13aa3b6c6e1b55e1480f4961
treesapelectronics <33070706+treesapelectronics@users.noreply.github.com>
2017-12-01 16:44:26 -0500

    EPIC
    The step sequencer is nearly complete!'
    
    Now only need tempo control, and saving functionalities!

--- finalproject_PROJECT_TREESAP.ino
+++ finalproject_PROJECT_TREESAP.ino
@@ -4,16 +4,20 @@ boolean grid[8][8] = {
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
-  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0},
   {0, 0, 0, 0, 0, 0, 0, 0}
 };
+
 boolean bState = 0;
 boolean lbState = 0;
-int lastCheck = 0;
+unsigned long lastCheck = 0;
 int checkInt = 20;
+int tempo = 0;
+int currentStep = 0;
+boolean stepState[8] = { false, false, false, false, false, false, false, false };
 
 LedControl lc = LedControl(12, 11, 10, 1); //10 is to CLOCK, 9 = CS, 8=DIN//
 
@@ -32,7 +36,7 @@ void loop() {
   const int LR = analogRead(A15);
   int x_translate = map(LR, 1023, 0, 7, -1); //This maps the values//
   int y_translate = map(UD, 0, 1023, 0, 7);
-  stepDot();
+  sequence();
   drawGrid();
   checkButton();
   lc.setLed(0, x_translate, y_translate, true);
@@ -44,7 +48,12 @@ void loop() {
 void drawGrid() {
   for (int i = 0; i <= 7; i++) {
     for (int j = 0; j <= 7; j++) {
+      if (j == currentStep) {
+        lc.setLed(0, i, j, true);
+      }
+      else{
       lc.setLed(0, i, j, grid[i][j]);
+      }
     }
   }
 }
@@ -54,9 +63,9 @@ void checkButton() {
   const int LR = analogRead(A15);
   int x_translate = map(LR, 1023, 0, 7, -1); //This maps the values//
   int y_translate = map(UD, 0, 1023, 0, 7);
-  if (millis() < lastCheck + checkInt) return;
-
-  lastCheck = millis();
+  //  if (millis() < lastCheck + checkInt) return;
+  //
+  //  lastCheck = millis();
 
   lbState = bState;
   bState = digitalRead(SW_pin);
@@ -65,14 +74,61 @@ void checkButton() {
   }
 }
 
-void stepDot() {
-  for (int i = 0; i <= 7; i++) {
-    for (int j = 0; j <= 7; j++) {
-      lc.setLed(0, i, j, true);
-      delay(20);
-      lc.setLed(0, i, j, false);
-      delay(20); //Mess with this delay to get your joystick correct//
+void sequence() {
+  tempo = 150;
+  if (millis() > lastCheck + (55350 / (tempo*2))) {  //if its time to go to the next step...
+    currentStep = currentStep + 1;         //increment to the next step
+    if (currentStep > 7) {
+      currentStep = 0;
     }
+    if (grid[0][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(60, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(60, 127, 1);
+    }
+    if (grid[1][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(61, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(61, 127, 1);
+    }
+    if (grid[2][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(62, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(62, 127, 1);
+    }
+    if (grid[3][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(63, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(63, 127, 1);
+    }
+    if (grid[4][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(64, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(64, 127, 1);
+    }
+    if (grid[5][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(65, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(65, 127, 1);
+    }
+    if (grid[6][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(66, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(66, 127, 1);
+    }
+    if (grid[7][currentStep] == HIGH) {
+      usbMIDI.sendNoteOn(67, 127, 1);
+    }
+    else {
+      usbMIDI.sendNoteOff(67, 127, 1);
+    }
+    lastCheck = millis();               //set lastStepTime to the current time
   }
 }
-
